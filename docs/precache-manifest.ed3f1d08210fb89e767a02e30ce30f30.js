de63ad43f156bccc7040197bf43caeccc4821c2b
Lucas Ospina A <lucasospina97@gmail.com>
2020-07-12 23:49:00 -0500

    git docs added

---
+++ docs/precache-manifest.ed3f1d08210fb89e767a02e30ce30f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f31dd593370cb94a54162ef5547560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac2fa135ed1ac2ef046",
+    "url": "/static/css/main.79b8f090.chunk.css"
+  },
+  {
+    "revision": "2c0adb474f2b2bda3f9b",
+    "url": "/static/js/2.688726de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.688726de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac2fa135ed1ac2ef046",
+    "url": "/static/js/main.0b44c412.chunk.js"
+  },
+  {
+    "revision": "caf821b007d82d3e9b1c",
+    "url": "/static/js/runtime-main.c1659c69.js"
+  }
+]);
